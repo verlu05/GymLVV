f58b014d9999a27e3a75598e59881ea855c4f3e3
verlu05 <verlu3003@gmail.com>
2026-09-12 21:12:40 +0200

    Clave anon correcta

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,1 +1,6 @@
-import { createClient } from "@supabase/supabase-js"; const supabaseUrl = "https://hyghznhixakllftrdrly.supabase.co"; const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5Z2h6bmhpeGFrbGxmdHJkcmx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyODEyNTIsImV4cCI6MjA1Njg1NzI1Mn0.eL39S1qg-J9f2K0K7XvP2W6M71nZ5Yq3aL8V4X0_b2k"; export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hyghznhixakllftrdrly.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnaWxvcmhpaGhsYmlzaXF1b2Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNDE0MDAsImV4cCI6MjEwNDcxNzQwMH0.3cJGqzP7HPGqn-P55dcHcZmX5ApTjgxhG4RaeprnVoU";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
