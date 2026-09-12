d2e18c5c47207687f72e39b1ce87aa9f02ebd41e
verlu05 <verlu3003@gmail.com>
2026-09-12 21:17:28 +0200

    URL y Key de Supabase correctas

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hyghznhixakllftrdrly.supabase.co";
+const supabaseUrl = "https://igilorhihhlbisiquofy.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnaWxvcmhpaGhsYmlzaXF1b2Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNDE0MDAsImV4cCI6MjEwNDcxNzQwMH0.3cJGqzP7HPGqn-P55dcHcZmX5ApTjgxhG4RaeprnVoU";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
